5d70c364d514bb2f620d08c2ed82bc6e31b0e58b
BansheeWin <garrydebyser8@gmail.com>
2017-02-07 17:29:20 +0100

    Fin première partie Module 5

--- DEV/JS/Module1/js/objet.js
+++ DEV/JS/Module1/js/objet.js
@@ -1,37 +1,27 @@
 'use strict';
 var voiture;
-var monthNames;
 var jour;
 var mois;
 var annee;
-
-voiture={};
-
-monthNames[0] = 'Janvier';
-monthNames[1] = 'Février';
-monthNames[2] = 'Mars';
-monthNames[3] = 'Avril';
-monthNames[4] = 'Mai';
-monthNames[5] = 'Juin';
-monthNames[6] = 'Juillet';
-monthNames[7] = 'Août';
-monthNames[8] = 'Septembre';
-monthNames[9] = 'Octobre';
-monthNames[10] = 'Novembre';
-monthNames[11] = 'Décembre';
-
-voiture.marque="Ford";
-voiture.annee="1969";
-voiture.datachat=new Date(2016,5,12);
-voiture.passage=new Array("Toto","Titi");
-
-
+voiture = new Object();
+voiture.marque = "Ford";
+voiture.annee = "1969";
+voiture.datachat = new Date('2016-5-12');
+voiture.passage = new Array("Toto", "Titi");
+/********************************************************************
+        
+            Récupération de la date et affectation aux variables
+            
+*********************************************************************/
 jour = voiture.datachat.getDate();
 mois = voiture.datachat.getMonth();
 annee = voiture.datachat.getFullYear();
-
-
-console.log(voiture.marque);
-console.log(voiture.annee);
-console.log(jour+" "+mois+" "+annee);
-console.log(voiture.passage);
+/********************************************************************
+        
+            Ecriture dans la page HTML
+            
+*********************************************************************/
+document.write("<p>Marque : " + voiture.marque + "</p>");
+document.write("<p>Année de fabrication : " + voiture.annee + "</p>");
+document.write("<p>Date d'achat : " + annee + "-" + mois + "-" + jour + "</p>");
+document.write("<p>Passagers : " + voiture.passage + "</p>");
