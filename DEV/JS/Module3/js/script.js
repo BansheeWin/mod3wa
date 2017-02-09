e673e14a32294cb9053224818c5509471e1183b9
BansheeWin <garrydebyser8@gmail.com>
2017-02-09 14:00:37 +0100

    Module 3

---
+++ DEV/JS/Module3/js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+var i;
+var numbers = [0, 1, 2, 3, 4,5, 6, 7, 8 ];
+//compteur=parseInt(window.prompt("Quel nombre souhaitez-vous ?",0));
+for(i=0;i<numbers.length;i++){
+    document.write("<p>"+numbers[i]+"</p>");
+}
