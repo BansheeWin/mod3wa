62ccb925387e3ff4d3355fa90c4458b3e726834c
BansheeWin <garrydebyser8@gmail.com>
2017-03-01 15:14:56 +0100

    Cup of Tea

---
+++ INTEGRATION/module11/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+var price;
+var btn;
+var cart;
+btn = document.querySelector('.btn');
+price = document.querySelector('.price h4');
+cart = document.querySelector('#cart strong');
+console.log(cart);
+console.log(price);
+//var turnObjToArray = function (obj) {
+//    return [].map.call(obj, function (element) {
+//        return element;
+//    })
+//};
+function addCart() {
+    var panier = Number(cart.dataset.prix);
+    var total = panier + Number(price.dataset.prix);
+    cart.innerHTML = formatPrice(total);
+    cart.dataset.prix = total;
+}
+
+function formatPrice(nb) {
+    return nb.toFixed(2).replace('.', ',') + ' €';
+}
+btn.addEventListener("click", addCart, false);
